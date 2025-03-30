2813d56aa327d557547fe280c4f8b62e649d953d
Nishia25 <nishi.a@peerbits.com>
2025-03-31 03:03:33 +0530

    delete acc

--- lib/src/accounts/accounts_pages/delete_account.dart
+++ lib/src/accounts/accounts_pages/delete_account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vision_intelligence/common/widgets/appbar.dart';
 
 class DeleteAccount extends StatelessWidget {
   const DeleteAccount({super.key});
@@ -6,7 +7,31 @@ class DeleteAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: Appbar(
+          title: "Delete Account",
+          showBackButton: true,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[850],
+        ),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: const EdgeInsets.only(top: 10, bottom: 30),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
+            color: Color.fromRGBO(255, 255, 255, 1),
+          ),
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+
+          ),
+        ),
+      ),
     );
   }
 }
